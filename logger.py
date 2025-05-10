19d77b5a920d36a4345c23345919a7f2d1504f1a
Hunt3r0x <106396603+Hunt3r0x@users.noreply.github.com>
2025-05-10 04:50:05 +0300

    Added logging system: Implemented file and console logging with colored output

---
+++ logger.py
@@ -0,0 +1,52 @@
+import logging
+import os
+from datetime import datetime
+from colorama import init, Fore, Style
+
+init(autoreset=True)
+
+class ColoredFormatter(logging.Formatter):
+    """Custom formatter that adds colors to the log messages."""
+    
+    COLORS = {
+        'DEBUG': Fore.BLUE,
+        'INFO': Fore.GREEN,
+        'WARNING': Fore.YELLOW,
+        'ERROR': Fore.RED,
+        'CRITICAL': Fore.RED + Style.BRIGHT
+    }
+
+    def format(self, record):
+        if record.levelname in self.COLORS:
+            record.levelname = f"{self.COLORS[record.levelname]}{record.levelname}{Style.RESET_ALL}"
+        return super().format(record)
+
+def setup_logger(log_dir='logs'):
+    """Set up the logger with both file and console handlers."""
+    
+    # Create logs directory if it doesn't exist
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+    
+    # Create logger
+    logger = logging.getLogger('DNSExfiltration')
+    logger.setLevel(logging.DEBUG)
+    
+    # Create file handler
+    log_file = os.path.join(log_dir, f'dns_exfiltration_{datetime.now().strftime("%Y%m%d_%H%M%S")}.log')
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setLevel(logging.DEBUG)
+    file_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(file_formatter)
+    
+    # Create console handler
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    console_formatter = ColoredFormatter('%(levelname)s - %(message)s')
+    console_handler.setFormatter(console_formatter)
+    
+    # Add handlers to logger
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+    
+    return logger 
